6385e56e3bb056af8a30a5b87fc885eb73859e25
jove <zjianfa@foxmail.com>
2021-08-17 22:54:00 +0800

    doc: add README.md

--- web-api/src/main/java/com/example/api/req/AddGroupReq.java
+++ web-api/src/main/java/com/example/api/req/AddGroupReq.java
@@ -2,10 +2,11 @@ package com.example.api.req;
 
 
 import com.example.api.req.common.BaseIdReq;
-import com.sun.istack.internal.NotNull;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
+
+import javax.validation.constraints.NotNull;
 
 
 /**
